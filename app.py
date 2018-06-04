cf5d7b1aab781e596bda2db27daa7b0c97ae3c9f
Gautham <gbommannan@managedbyq.com>
2018-06-04 10:17:00 -0400

    bot that adds users back. it is evil. just like rabbits

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-A routing layer for the onboarding bot tutorial built using
-[Slack's Events API](https://api.slack.com/events-api) in Python
-"""
 import json
 import bot
 from flask import Flask, request, make_response, render_template
@@ -31,42 +27,12 @@ def _event_handler(event_type, slack_event):
         Response object with 200 - ok or 500 - No Event Handler error
 
     """
-    team_id = slack_event["team_id"]
-    # ================ Team Join Events =============== #
-    # When the user first joins a team, the type of event will be team_join
-    if event_type == "team_join":
-        user_id = slack_event["event"]["user"]["id"]
-        # Send the onboarding message
-        pyBot.onboarding_message(team_id, user_id)
-        return make_response("Welcome Message Sent", 200,)
-
-    # ============== Share Message Events ============= #
-    # If the user has shared the onboarding message, the event type will be
-    # message. We'll also need to check that this is a message that has been
-    # shared by looking into the attachments for "is_shared".
-    elif event_type == "message" and slack_event["event"].get("attachments"):
-        user_id = slack_event["event"].get("user")
-        if slack_event["event"]["attachments"][0].get("is_share"):
-            # Update the onboarding message and check off "Share this Message"
-            pyBot.update_share(team_id, user_id)
-            return make_response("Welcome message updates with shared message",
-                                 200,)
-
-    # ============= Reaction Added Events ============= #
-    # If the user has added an emoji reaction to the onboarding message
-    elif event_type == "reaction_added":
-        user_id = slack_event["event"]["user"]
-        # Update the onboarding message
-        pyBot.update_emoji(team_id, user_id)
-        return make_response("Welcome message updates with reactji", 200,)
-
-    # =============== Pin Added Events ================ #
-    # If the user has added an emoji reaction to the onboarding message
-    elif event_type == "pin_added":
-        user_id = slack_event["event"]["user"]
-        # Update the onboarding message
-        pyBot.update_pin(team_id, user_id)
-        return make_response("Welcome message updates with pin", 200,)
+    if event_type == "member_left_channel" and slack_event["event"]["channel"] == "C08AS1ARH":
+        try:
+            user_id = slack_event["event"]["user"]
+            pyBot.bring_back_user(user_id, slack_event["event"]["channel"], slack_event["token"])
+        except KeyError:
+            pass
 
     # ============= Event Type Not Found! ============= #
     # If the event_type does not have a handler
@@ -97,6 +63,7 @@ def thanks():
     """
     # Let's grab that temporary authorization code Slack's sent us from
     # the request's parameters.
+    print("ON THANKS PAGE")
     code_arg = request.args.get('code')
     # The bot's auth method to handles exchanging the code for an OAuth token
     pyBot.auth(code_arg)
@@ -138,7 +105,7 @@ def hears():
         # Then handle the event by event_type and have your bot respond
         return _event_handler(event_type, slack_event)
     # If our bot hears things that are not events we've subscribed to,
-    # send a quirky but helpful error response
+    # send a helpful error response
     return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
                          you're looking for.", 404, {"X-Slack-No-Retry": 1})
 
